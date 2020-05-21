d56f5ac6d6ff43798d3f868c5e2f8d042a790426
reimibeta <pinsoneatra.soneatra846@gmail.com>
2020-05-21 15:44:45 +0700

    Update admin.py

--- characters/admin.py
+++ characters/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Characters, Anime, Weebs
+from .models import Characters, Anime, Cosplays
 
 admin.site.register(Characters)
 admin.site.register(Anime)
-admin.site.register(Weebs)
+admin.site.register(Cosplays)
