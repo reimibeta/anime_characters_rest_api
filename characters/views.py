f24066745269d4b78ddc65bfe9c69c1469110b18
reimibeta <65539218+reimibeta@users.noreply.github.com>
2020-05-21 21:00:59 +0700

    Update views.py

--- characters/views.py
+++ characters/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Characters, Anime, Cosplays
 from .serializers import CharactersSerializer, AnimeSerializer, CosplaySerializer
 
 class CharactersView(viewsets.ModelViewSet):
     queryset = Characters.objects.all()
     serializer_class = CharactersSerializer
+    permission_classs = (permissions.IsAuthenticatedOrReadOnly)
 
 class AnimeView(viewsets.ModelViewSet):
     queryset = Anime.objects.all()
