9f04d38237421174f3bc54d497cc01e9b97a5444
reimibeta <pinsoneatra.soneatra846@gmail.com>
2020-05-21 11:20:40 +0700

    Update views.py

--- characters/views.py
+++ characters/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Characters
+from .serializers import CharactersSerializer
 
-# Create your views here.
+class CharactersView(viewsets.ModelViewSet):
+    queryset = Characters.objects.all()
+    serializer_class = CharactersSerializer
