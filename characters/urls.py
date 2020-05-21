28fb76bf868313012c408d4d1cffc2526c64903d
reimibeta <pinsoneatra.soneatra846@gmail.com>
2020-05-21 11:20:38 +0700

    Update urls.py

--- characters/urls.py
+++ characters/urls.py
@@ -1,6 +1,10 @@
-from django.contrib import admin
 from django.urls import path, include
+from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('characters', views.CharactersView)
 
 urlpatterns = [
-    # path('', include('characters.urls')),
+    path('', include(router.urls)),
 ]
