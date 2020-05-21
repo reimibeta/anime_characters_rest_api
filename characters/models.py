8f9d4b0371c37e40940536098db5aa8f56dfc25e
reimibeta <pinsoneatra.soneatra846@gmail.com>
2020-05-21 10:34:51 +0700

    Update models.py

--- characters/models.py
+++ characters/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Characters(models.Model):
+    name = models.CharField(max_length=50)
+    anime = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name_plural = 'Characters'
