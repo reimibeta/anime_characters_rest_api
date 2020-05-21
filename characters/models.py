ea7452f5b30bf6d7b75b951680df360146af3128
reimibeta <pinsoneatra.soneatra846@gmail.com>
2020-05-21 13:35:28 +0700

    Update models.py

--- characters/models.py
+++ characters/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
+class Anime(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
 class Characters(models.Model):
     name = models.CharField(max_length=50)
-    anime = models.CharField(max_length=50)
-
+    anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
     def __str__(self):
         return self.name
     class Meta:
         verbose_name_plural = 'Characters'
+
+class Weebs(models.Model):
+    name = models.CharField(max_length=50)
+    characters = models.ManyToManyField(Characters)
+    def __str__(self):
+        return self.name
