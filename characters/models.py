63e34935a296ea119be372b0874429c32b8b3e4b
reimibeta <pinsoneatra.soneatra846@gmail.com>
2020-05-21 15:44:52 +0700

    Update models.py

--- characters/models.py
+++ characters/models.py
@@ -5,6 +5,9 @@ class Anime(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = 'Anime'
+
 class Characters(models.Model):
     name = models.CharField(max_length=50)
     anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
@@ -13,8 +16,11 @@ class Characters(models.Model):
     class Meta:
         verbose_name_plural = 'Characters'
 
-class Weebs(models.Model):
+class Cosplays(models.Model):
     name = models.CharField(max_length=50)
     characters = models.ManyToManyField(Characters)
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name_plural = 'Cosplays'
