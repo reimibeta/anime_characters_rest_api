1537fb452c5213bf7ffff9958dcf427dd6d2e4ef
reimibeta <65539218+reimibeta@users.noreply.github.com>
2020-05-21 20:58:22 +0700

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('characters.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
